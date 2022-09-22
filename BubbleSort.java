5cb9f23126ffc083f410d740289a26ecfa6a5581
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-22 12:50:58 +0200

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,18 +1,19 @@
 package org.example;
 
 class BubbleSort {
-    public void bubbleSortMethod(int arr[]) {
-        for (int i = 0; i < arr.length - 1; i++)
-            for (int j = 0; j < arr.length - i - 1; j++)
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+    public void bubbleSortMethod(int array[]) {
+        for (int i = 0; i < array.length - 1; i++)
+            for (int j = 0; j < array.length - i - 1; j++)
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
     }
-    public void printArray(int arr[]) {
-        for (int i = 0; i < arr.length; ++i)
-            System.out.print(arr[i] + " ");
+    public void printArray(int array[]) {
+        for (int i = 0; i < array.length; ++i)
+            System.out.print(array[i] + " ");
         System.out.println();
     }
 }
+
