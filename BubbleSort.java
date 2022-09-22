a0f3dede840792cb16cd6cdbe16dab66a0f86363
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-22 12:50:17 +0200

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,18 @@
+package org.example;
+
+class BubbleSort {
+    public void bubbleSortMethod(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++)
+            for (int j = 0; j < arr.length - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+    public void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
